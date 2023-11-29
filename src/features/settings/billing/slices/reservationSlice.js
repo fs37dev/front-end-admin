a9d765f22b61c93571bf8f5e558664d4efe8b6a7
unknown <heryrhamadan@gmail.com>
2023-11-30 04:48:34 +0700

    refactor: routes

--- src/features/settings/billing/slices/reservationSlice.js
+++ src/features/settings/billing/slices/reservationSlice.js
@@ -30,11 +30,9 @@ export const getReservationDetail = createAsyncThunk("getReservationDetail", asy
 export const updateReservation = createAsyncThunk("updateReservation", async ({ id, status }) => {
   try {
     const response = await axios.put(`${API_URL}${id}`, { status: status });
-    console.info(response.data);
-    // return await response.data;
+    return await response.data;
   } catch (err) {
-    // return await err.response.data;
-    console.info(err.response.data);
+    return await err.response.data;
   }
 });
 
@@ -73,6 +71,16 @@ const reservationSlice = createSlice({
         state.reservations = [];
       })
       .addCase(getReservationDetail.rejected, (state, action) => {
+        state.loading = true;
+      })
+      .addCase(updateReservation.pending, (state, action) => {
+        state.loading = true;
+      })
+      .addCase(updateReservation.fulfilled, (state, action) => {
+        state.loading = false;
+        window.location.reload();
+      })
+      .addCase(updateReservation.rejected, (state, action) => {
         state.loading = true;
       });
   },
